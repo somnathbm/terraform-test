aa131b1277054dae31d4ede155c21d92ed736146
somnathbm <somnathbm0@gmail.com>
2023-07-31 12:53:03 +0530

    instance variable validation added

--- aws-ec2/variables.tf
+++ aws-ec2/variables.tf
@@ -2,6 +2,10 @@ variable "instance_type" {
   type        = string
   description = "EC2 instance type"
   default     = "t2.micro"
+  validation {
+    condition = can(regex("^t2\\.", var.instance_type))
+    error_message = "The instance must be of t2 generation"
+  }
 }
 
 variable "sec_ebs_size" {
